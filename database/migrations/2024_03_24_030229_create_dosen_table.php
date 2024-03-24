7767ce5453fed178cf3db33657fa4a0e7a3255aa
asrulabdullah99 <srulabdullah@gmail.com>
2024-03-24 10:15:25 +0700

    update migration

---
+++ database/migrations/2024_03_24_030229_create_dosen_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('dosen', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('nidn')->unique();
+            $table->string('nama_dosen');
+            $table->enum('jenis_kelamin', ['L', 'P']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('dosen');
+    }
+};
