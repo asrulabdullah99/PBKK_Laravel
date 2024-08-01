f8144df91f0f8f2481fd49e7d37cfb7e89b6aaea
asrulabdullah99 <srulabdullah@gmail.com>
2024-08-02 06:39:42 +0700

    update api in laravel

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Auth\Middleware\Authenticate;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+Route::get('/users', function (Request $request) {
     return $request->user();
 })->middleware(Authenticate::using('sanctum'));
 
